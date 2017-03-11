a20851a960412526fd018560a13ee20cb829ad3f
武沛齐 <wupeiqi@haodf.com>
2017-03-11 10:07:12 +0800

    sys.path

--- cmdb/AutoClient/bin/auto-client.py
+++ cmdb/AutoClient/bin/auto-client.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 import os
+import sys
 
 BASEDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-os.path.join(BASEDIR)
+sys.path.append(BASEDIR)
+
 from src.scripts import client
 
 if __name__ == '__main__':
